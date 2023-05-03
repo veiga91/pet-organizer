8b2be2346d99c1309d0c368c90996ce5c80e40b4
Wanderson Veiga <wanderalez@gmail.com>
2023-05-03 14:28:58 +0100

    add route names

---
+++ src/components/routes.ts
@@ -0,0 +1,8 @@
+export enum Routes {
+    HOME = '/',
+    PET_DETAIL = '/pet-detail',
+    PROFILE = '/profile',
+    SIGN_IN = '/sign-in',
+    SIGN_OUT = '/sign-out',
+    SIGN_UP = '/sign-up'
+}
