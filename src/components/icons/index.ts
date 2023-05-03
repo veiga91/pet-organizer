28f8bb604ff75baa8104ee7874ff310f9c6aa83d
Wanderson Veiga <wanderalez@gmail.com>
2023-05-03 14:25:23 +0100

    add assets

---
+++ src/components/icons/index.ts
@@ -0,0 +1,8 @@
+export {default as addPet} from './addPet.png'
+export {default as arrowRight} from './arrowRight.svg'
+export {default as grooming} from './grooming.png'
+export {default as home} from './home.png'
+export {default as logo} from './logo.png'
+export {default as petFood} from './petFood.png'
+export {default as petHealth} from './petHealth.png'
+export {default as signUp} from './signUp.png'
